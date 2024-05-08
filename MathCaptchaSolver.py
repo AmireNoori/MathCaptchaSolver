c4550de45da373cc27366d259084834c9ede2582
Amir Noori <100022896+AmireNoori@users.noreply.github.com>
2024-05-09 02:01:49 +0330

    Update MathCaptchaSolver.py

--- MathCaptchaSolver.py
+++ MathCaptchaSolver.py
@@ -11,51 +11,53 @@ class CaptchaSolver:
 
     def enhance_legibility(self, cropped_image):
         gray = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
-        _, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        return cv2.erode(cv2.blur(mask, (2, 2)), self.kernel, iterations=1)
+        return gray
 
-    def math_operation(self, left_number, right_number, operation='+'):
-        if left_number.isdigit() and right_number.isdigit():
-            return eval(f"{left_number} {operation} {right_number}")
-        else:
-            return None
-    def math_operation_for_both_signs(self, left_number, right_number):
-        if left_number.isdigit() and right_number.isdigit():
+    def math_operation(self, left_number, right_number):
+        if right_number.isdigit():
+            return eval(f"{left_number} + {right_number}")
+        return None
+
+    def resolve(self, left_image, right_image,left_image_twice,right_image_twice):
+        left_number = pipe(left_image_twice)[0]['generated_text']
+        if left_number.isdigit():
             left_number = int(left_number)
-            right_number = int(right_number)
-            return [left_number - right_number, left_number + right_number]
+            if left_number<10 or left_number==None or left_number=="":
+                left_number = pipe(left_image)[0]['generated_text']
+                right_number = pipe(right_image_twice)[0]['generated_text']
+                if right_number.isdigit() and int(right_number)>10:
+                    return self.math_operation(left_number, right_number)
+                else:
+                    right_number = pipe(right_image)[0]['generated_text']
+                    return self.math_operation(left_number, right_number)
+            elif left_number>=10 :
+                right_number = pipe(right_image_twice)[0]['generated_text']
+                return self.math_operation(left_number, right_number)
         else:
-            return None
-    def resolve(self, left_image, right_image, sign_image, negative_sign_right_image):
-        sign = pipe(sign_image)[0]['generated_text']
-        left_number = pipe(left_image)[0]['generated_text']
-        
-        if sign in {'+', '@', '4','*'}:
-            right_number = pipe(right_image)[0]['generated_text']
-            return self.math_operation(left_number, right_number)
-        elif sign in {'-', '='}:
-            right_number = pipe(negative_sign_right_image)[0]['generated_text']
-            return self.math_operation(left_number, right_number, '-')
-        else:
-            unfixed_right_number = ''.join(char for char in pipe(right_image)[0]['generated_text'] if char.isdigit())
-            return self.math_operation_for_both_signs(left_number, unfixed_right_number)
+            left_number = pipe(left_image)[0]['generated_text']
+            if left_number.isdigit():
+                right_number = pipe(right_image)[0]['generated_text']
+                return self.math_operation(left_number, right_number)
+
 
     def solve_captcha(self):
-        positions = {'left': 5, 'right': 60, 'sign': 39, 'negative_sign_right': 56}
-        dimensions = {'width': 25, 'height': 20, 'width_sign': 15, 'height_sign': 15, 'width_negative_sign': 18}
+        positions = {'left': 5, 'right_unit': 57 , 'right_twice' : 71}
+        dimensions = {'width_twice': 31, 'width_unit': 19, 'height': 20}
         
-        left_image = self.image[7:27, positions['left']:positions['left']+dimensions['width']]
-        right_image = self.image[7:27, positions['right']:positions['right']+dimensions['width']]
-        sign_image = self.image[10:25, positions['sign']:positions['sign']+dimensions['width_sign']]
-        negative_sign_right_image = self.image[7:27, positions['negative_sign_right']:positions['negative_sign_right']+dimensions['width_negative_sign']]
-        
-        left_enhanced = self.enhance_legibility(left_image)
-        right_enhanced = self.enhance_legibility(right_image)
-        negative_sign_right_enhanced = self.enhance_legibility(negative_sign_right_image)
+        left_image_for_unit_number = self.image[7:30, positions['left']:positions['left']+dimensions['width_unit']]
+        left_image_for_twice_number = self.image[7:30, positions['left']:positions['left']+dimensions['width_twice']]
+        right_image_for_left_twice_number = self.image[7:30, positions['right_twice']:positions['right_twice']+dimensions['width_twice']]
+        right_image_for_left_unit_number = self.image[7:30, positions['right_unit']:positions['right_unit']+dimensions['width_twice']]
 
-        cv2.imwrite('left_number.jpg', left_enhanced)
-        cv2.imwrite('right_number.jpg', right_enhanced)
-        cv2.imwrite('sign.jpg', sign_image)
-        cv2.imwrite('negative_sign_right_number.jpg', negative_sign_right_enhanced)
+        left_enhanced = self.enhance_legibility(left_image_for_unit_number)
+        left_enhanced_for_twice_number = self.enhance_legibility(left_image_for_twice_number)
+        right_enhanced = self.enhance_legibility(right_image_for_left_unit_number)
+        right_enhanced_for_twice_number = self.enhance_legibility(right_image_for_left_twice_number)
 
-        return self.resolve('left_number.jpg', 'right_number.jpg', 'sign.jpg', 'negative_sign_right_number.jpg')
+        cv2.imwrite('left_number.png', left_enhanced)
+        cv2.imwrite('left_image_for_twice_number.png', left_enhanced_for_twice_number)
+        cv2.imwrite('right_number.png', right_enhanced)
+        cv2.imwrite('right_image_for_twice_number.png', right_enhanced_for_twice_number)
+
+
+        return self.resolve('left_number.png', 'right_number.png','left_image_for_twice_number.png',"right_image_for_twice_number.png")
